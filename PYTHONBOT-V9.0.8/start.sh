c41dabec707b6f6cb601ba5ce8a12c5a43433faf
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-08 07:52:43 -0700

    Update start.sh

--- PYTHONBOT-V9.0.8/start.sh
+++ PYTHONBOT-V9.0.8/start.sh
@@ -1,4 +1,4 @@
-#   Copyright (C) 2021 LEGEND-OS
+#   Copyright (C) 2021 LEGEND-LX
 #
 #   Poject VΛDΣЯ is free software: 
 #   you can redistribute it and/or modify it under the terms of the GNU General Public License 
@@ -16,8 +16,8 @@
 
 echo """
               
-Legendary LEGENDBOT!!
-@Legend_Userbot
+Legendary PYTHONBOT!!
+@Python_Userbot_Support
 """
 
 python3 -m userbot
