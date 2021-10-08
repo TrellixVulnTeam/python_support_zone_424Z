ee3cbb49d110804e46e0d6736e05253b77cc1645
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-07 21:38:32 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ echo """
                                
 """
 rm -rf  python_support_zone
-git clone https://github.com/LEGEND-OS/LEGENDUSERBOT
-cd LEGENDUSERBOT
+git clone https://github.com/LEGEND-LX/python_support_zone
+cd python_support_zone
 python3 -m userbot
