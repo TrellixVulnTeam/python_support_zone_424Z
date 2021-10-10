7ab4ebfd94189e8c6d1686571ebcec446492c2c3
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-10 07:47:16 -0700

    Update _helper.py

--- userbot.py/plugins/_helper.py
+++ userbot.py/plugins/_helper.py
@@ -6,9 +6,9 @@ from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot, BotInl
 from userbot.Config import Config
 from . import *
 from userbot import ALIVE_NAME, CMD_LIST, SUDO_LIST
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from PYTHONBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 
-perf = "[ †hê Lêɠêɳ̃dẞø† ]"
+perf = "[ DMG-Pythonẞø† ]"
 
 mybot = Config.BOT_USERNAME
 if mybot.startswith("@"):
@@ -18,12 +18,12 @@ else:
 
 
 msg = f"""
-**⚜ 𝙻𝚎𝚐𝚎𝚗𝚍𝚊𝚛𝚢 𝙰𝚏 𝙻𝚎𝚐𝚎𝚗𝚍𝙱𝚘𝚝 ⚜**
+**⚜ 𝙻𝚎𝚐𝚎𝚗𝚍𝚊𝚛𝚢 𝙰𝚏 Python𝙱𝚘𝚝 ⚜**
 
-  •        [♥️ 𝚁𝚎𝚙𝚘 ♥️](https://github.com/LEGEND-OS/LEGENDBOT)
+  •        [♥️ 𝚁𝚎𝚙𝚘 ♥️](https://github.com/LEGEND-LX/PYTHONBOT-V9.0.8)
   •        [♦️ Deploy ♦️](https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FLEGEND-OS%2FLEGENDBOT&template=https%3A%2F%2Fgithub.com%2FLEGEND-OS%2FLEGENDBOT)
 
-  •  ©️ {Legend_channel} ™
+  •  ©️ {Python_channel} ™
 """
 botname = Config.BOT_USERNAME
 
@@ -32,15 +32,15 @@ botname = Config.BOT_USERNAME
 async def repo(event):
     try:
         legend = await bot.inline_query(botname, "repo")
-        await legend[0].click(event.chat_id)
-        if event.sender_id == Its_LegendBoy:
+        await python[0].click(event.chat_id)
+        if event.sender_id == Legendl_Mr_Hacker:
             await event.delete()
     except (noin, dedbot):
         await eor(event, msg)
 
 
-@bot.on(admin_cmd(pattern="help ?(.*)", outgoing=True))
-@bot.on(sudo_cmd(pattern="help ?(.*)", allow_sudo=True))
+@bot.on(admin_cmd(pattern="python ?(.*)", outgoing=True))
+@bot.on(sudo_cmd(pattern="python ?(.*)", allow_sudo=True))
 async def _(event):
     if event.fwd_from:
         return
@@ -48,7 +48,7 @@ async def _(event):
     chat = "@Botfather"
     if tgbotusername is not None:
         try:
-            results = await event.client.inline_query(tgbotusername, "legendbot_help")
+            results = await event.client.inline_query(tgbotusername, "pythonbot_help")
             await results[0].click(
                 event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
             )
@@ -79,8 +79,8 @@ async def yardim(event):
         return
     tgbotusername = botname
     input_str = event.pattern_match.group(1)
-    if tgbotusername is not None or LEGEND_input == "text":
-        results = await event.client.inline_query(tgbotusername, "legendbot_help")
+    if tgbotusername is not None or PYTHON_input == "text":
+        results = await event.client.inline_query(tgbotusername, "pythonbot_help")
         await results[0].click(
             event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
         )
@@ -100,7 +100,7 @@ async def yardim(event):
 
 @bot.on(admin_cmd(pattern="plinfo(?: |$)(.*)", outgoing=True))
 @bot.on(sudo_cmd(pattern="plinfo(?: |$)(.*)", allow_sudo=True))
-async def legendbott(event):
+async def pythonbott(event):
     if event.fwd_from:
         return
     args = event.pattern_match.group(1).lower()
