508c31046b92299ffd117285ef27dcb82663716b
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-16 10:01:36 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 echo """
                                
 """
-rm -rf  python_support_zone
+rm -rf python_support_zone
 git clone https://github.com/LEGEND-LX/python_support_zone
 cd python_support_zone
 python3 -m userbot
