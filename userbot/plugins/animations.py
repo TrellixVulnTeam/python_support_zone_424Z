f5698df2fec4301c031dc6258005224b84fa4b56
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-22 23:29:04 -0700

    Update animations.py

--- userbot/plugins/animations.py
+++ userbot/plugins/animations.py
@@ -3,7 +3,7 @@ import asyncio
 from telethon.tl.functions.users import GetFullUserRequest
 
 from userbot import *
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from PYTHONBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "ν2.ο"
