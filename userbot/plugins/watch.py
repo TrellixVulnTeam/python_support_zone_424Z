56d6bd74d25b84c21571f6c6ae566887f7e52ee8
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-23 01:12:00 -0700

    Update watch.py

--- userbot/plugins/watch.py
+++ userbot/plugins/watch.py
@@ -7,7 +7,7 @@
 
 from justwatch import JustWatch
 
-from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from PYTHONBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 from userbot.cmdhelp import CmdHelp
 
 
