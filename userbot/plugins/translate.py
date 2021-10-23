a08324ad24dffe8ead892582fe44cc2e1d7d5800
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-23 01:24:13 -0700

    Update translate.py

--- userbot/plugins/translate.py
+++ userbot/plugins/translate.py
@@ -6,7 +6,7 @@ Available Commands:
 import emoji
 from googletrans import Translator
 
-from LEGENDBOT.utils import *
+from PYTHONBOT.utils import *
 from userbot.cmdhelp import CmdHelp
 
 @bot.on(admin_cmd(pattern="trt ?(.*)"))
