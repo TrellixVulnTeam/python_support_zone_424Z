015d771cfc5df2371635f248afd3d0b5031d8ee6
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-24 08:19:16 -0700

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -5,7 +5,7 @@ from telethon.errors import ChatSendInlineForbiddenError as noin
 from telethon.errors.rpcerrorlist import BotMethodInvalidError as dedbot
 from userbot.Config import Config
 from telethon import version
-from userbot import ALIVE_NAME, StartTime, PYTHONversion
+from userbot import ALIVE_NAME, StartTime, LEGENDversion
 from PYTHONBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 from . import *
@@ -18,14 +18,14 @@ async def reply_id(event):
     return reply_to_id
 
 
-DEFAULTUSER = ALIVE_NAME or "✞︎t͛ẞ̸ Pythonẞø✞︎ 🇮🇳"
-PYTHON_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.ALIVE_MSG or "ℓєgєи∂-ℓx Choice Pythonẞø✞︎"
-CUSTOM_YOUR_GROUP =Config.YOUR_GROUP or "@Python_Userbot_Support"
+DEFAULTUSER = ALIVE_NAME or "✞t͛ẞ̸ 𝖑𝖊ɠêɳ̃dẞø✞ 🇮🇳"
+LEGEND_IMG = Config.ALIVE_PIC
+CUSTOM_ALIVE_TEXT = Config.ALIVE_MSG or "ℓєgєи∂ Choice 𝖑𝖊ɠêɳ̃dẞø✞"
+CUSTOM_YOUR_GROUP =Config.YOUR_GROUP or "@Legend_Userbot"
 
-Python = bot.uid
+Legend = bot.uid
 
-mention = f"[{DEFAULTUSER}](tg://user?id={Python})"
+mention = f"[{DEFAULTUSER}](tg://user?id={Legend})"
 
 
 def get_readable_time(seconds: int) -> str:
@@ -59,95 +59,69 @@ def get_readable_time(seconds: int) -> str:
 uptime = get_readable_time((time.time() - StartTime))
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="legend$"))
+@bot.on(sudo_cmd(pattern="legend$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
     reply_to_id = await reply_id(alive)
 
-    if  PYTHON_IMG:
-        PYTHON_caption = f"**{CUSTOM_ALIVE_TEXT}**\n"
+    if  LEGEND_IMG:
+        LEGEND_caption = f"{CUSTOM_ALIVE_TEXT}**\n"
         
-        PYTHON_caption += f" ______𝙿𝚈𝚃𝙷𝙾𝙽-𝐔𝐬𝐞𝐫𝐛𝐨𝐭______\n"
-        PYTHON_caption += f"╔════❰🐍 Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ🐍 ❱═❍⊱❁۪۪¤๋͜\n"
-        PYTHON_caption += f"║╭━━━━━━━━━━━━━━━➣ \n"
-        PYTHON_caption += f"║┣⪼Developer^By┣⪼ [Legend-Lx](t.me/Legendl_Mr_Hacker) \n"
-        PYTHON_caption += f"║┣⪼Edition^Developer^By ┣⪼ [Legend-Os](t.me/The_LegendBoy) \n"
-        PYTHON_caption += f"║┣⪼ Ⲟⲱⲛⲉʀ     ┣⪼ [ℓєgєи∂-ℓx](t.me/Legendl_Mr_Hacker) \n"
-        PYTHON_caption += f"║┣⪼ Ⲋⲧⲁⲧυⲋ      ┣⪼ Ⲟⲛⳑⲓⲛⲉ\n"
-        PYTHON_caption += f"║┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ ┣⪼ {mention}\n"
-        PYTHON_caption += f"║┣⪼ Ⳙⲣⲧⲓⲙⲉ      ┣⪼  {uptime}\n"
-        PYTHON_caption += f"║┣⪼ Ⲃⲟⲧ Ⲣⲓⲛⳋ    ┣⪼  {uptime}\n"   
-        PYTHON_caption += f"║┣⪼ Ⲣⲩⲧⲏⲟⲛ      ┣⪼  {PYTHONversion}\n"
-        PYTHON_caption += f"║┣⪼`Os:`        ┣⪼   Kali GNU/Linux Rolling x86_64 \n"
-        PYTHON_caption += f"║┣⪼ Ⲧⲉⳑⲉⲧⲏⲟⲛ     ┣⪼  {version.__version__} \n"
-        PYTHON_caption += f"║┣⪼ [✨🐍PYTHON┣⪼ 𝐔𝐬𝐞𝐫𝐛𝐨𝐭🐍✨](https://t.me/Python_Userbot_Support)\n"
-        PYTHON_caption += f"║╰━━━━━━━━━━━━━━━➣ \n"
-        PYTHON_caption += f"╚══════════════════❍⊱❁۪۪¤๋͜-  \n"
+        LEGEND_caption += f"~~~~~~~~~~~~~~~~~~~~~~~\n"
+        LEGEND_caption += f"        ♥️ẞø† ẞ✞α†µѕ** \n"
+        LEGEND_caption += f"•⚜️• Øաղ̃ҽ̈ɾ          : {mention}\n\n"
+        LEGEND_caption += f"•📍• 𝖑𝖊ɠêɳ̃dẞø†   : {LEGENDversion}\n"
+        LEGEND_caption += f"•📍• †ҽ̀lҽ́thøղ̃     : `{version.version}`\n"
+        LEGEND_caption += f"•📍• 𝚄ρtime         : `{uptime}`\n"
+        LEGEND_caption += f"•📍• 𝙶𝚛𝚘𝚞𝚙           : [𝙶𝚛𝚘𝚞𝚙](t.me/Legend_Userbot)\n"
+        LEGEND_caption += f"•📍• 𝙼𝚢 𝙶𝚛𝚘𝚞𝚙  : {CUSTOM_YOUR_GROUP}\n"   
 
         await alive.client.send_file(
-            alive.chat_id, PYTHON_IMG, caption=PYTHON_caption, reply_to=reply_to_id
+            alive.chat_id, LEGEND_IMG, caption=LEGEND_caption, reply_to=reply_to_id
         )
         await alive.delete()
     else:
         await edit_or_reply(
             alive,
-            f"**{CUSTOM_ALIVE_TEXT}**\n\n"
-            f"______𝙿𝚈𝚃𝙷𝙾𝙽-𝐔𝐬𝐞𝐫𝐛𝐨𝐭______\n"
-            f"╔════❰🐍 Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ🐍 ❱═❍⊱❁۪۪¤๋͜\n"
-            f"║╭━━━━━━━━━━━━━━━➣ \n"
-            f"║┣⪼Developer^By┣⪼ [Legend-Lx](t.me/Legendl_Mr_Hacker) \n"
-            f"║┣⪼Edition^Developer^By ┣⪼ [Legend-Os](t.me/The_LegendBoy) \n"
-            f"║┣⪼Ⲟⲱⲛⲉʀ      ┣⪼ [ℓєgєи∂-ℓx](t.me/Legendl_Mr_Hacker) \n"
-            f"║┣⪼Ⲋⲧⲁⲧυⲋ      ┣⪼ Ⲟⲛⳑⲓⲛⲉ\n"
-            f"║┣⪼Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ ┣⪼ {mention}\n"
-            f"║┣⪼Ⳙⲣⲧⲓⲙⲉ      ┣⪼  {uptime}\n"
-            f"║┣⪼Ⲃⲟⲧ Ⲣⲓⲛⳋ     ┣⪼  {uptime}\n"   
-            f"║┣⪼Ⲣⲩⲧⲏⲟⲛ       ┣⪼  {PYTHONversion}\n"
-            f"║┣⪼`Os:`        ┣⪼   Kali GNU/Linux Rolling x86_64 \n"                                       
-            f"║┣⪼Ⲧⲉⳑⲉⲧⲏⲟⲛ     ┣⪼  {version.__version__} \n"
-            f"║┣⪼[✨🐍PYTHON┣⪼ 𝐔𝐬𝐞𝐫𝐛𝐨𝐭🐍✨](https://t.me/Python_Userbot_Support)\n"
-            f"║╰━━━━━━━━━━━━━━━➣ \n"
-            f"╚══════════════════❍⊱❁۪۪¤๋͜-  \n"
-         )
+            f"{CUSTOM_ALIVE_TEXT}**\n\n"
+            f"~~~~~~~~~~~~~~~~~~~~~~~ \n"
+            f"         𝕭𝖔𝖙 𝕾𝖙𝖆𝖙𝖚𝖘\n"
+            f"•⚡️• 𝕿єℓєτнοи    : `{version.__version__}`\n"
+            f"🇮🇳 ℓєgєи∂ϐοτ  : `{LEGENDversion}`\n"
+            f"🇮🇳 υρτιмє        : `{uptime}`\n"
+            f"🔱 ɱαรƭεɾ        : {mention}\n"
+            f"🔱 σωɳεɾ         : [ℓєgєи∂](t.me/Its_LegendBoy)\n"
+        )
 
 
 msg = f"""
-**  ⚜️ Pythonẞø† ιѕ σиℓιиє ⚜️**
-
+  ⚜️ Lêɠêɳ̃dẞø† ιѕ σиℓιиє ⚜️
 {Config.ALIVE_MSG}
-
-** ______𝙿𝚈𝚃𝙷𝙾𝙽-𝐔𝐬𝐞𝐫𝐛𝐨𝐭______\n"
-**╔════❰🐍 Ⲃⲟⲧ Ⲓⲛϝⲟʀⲙⲁⲧⲓⲟⲛ🐍 ❱═❍⊱❁۪۪¤๋͜\n"
-**║╭━━━━━━━━━━━━━━━➣ \n"
-**║┣⪼Developer^By┣⪼ [Legend-Lx](t.me/Legendl_Mr_Hacker) \n"
-**║┣⪼Edition^Developer^By ┣⪼ [Legend-Os](t.me/The_LegendBoy) \n"
-**║┣⪼ Ⲟⲱⲛⲉʀ     ┣⪼ [ℓєgєи∂-ℓx](t.me/Legendl_Mr_Hacker) \n"
-**║┣⪼ Ⲋⲧⲁⲧυⲋ      ┣⪼ Ⲟⲛⳑⲓⲛⲉ\n"
-**║┣⪼ Ⲃⲟⲧ Ⳳⲉʀⲋⲓⲟⲛ ┣⪼ {mention}\n"
-**║┣⪼ Ⳙⲣⲧⲓⲙⲉ      ┣⪼  {uptime}\n"
-**║┣⪼ Ⲃⲟⲧ Ⲣⲓⲛⳋ    ┣⪼  {uptime}\n"   
-**║┣⪼ Ⲣⲩⲧⲏⲟⲛ      ┣⪼  {PYTHONversion}\n"
-**║┣⪼`Os:`┣⪼  Kali GNU/Linux Rolling x86_64   \n"                                       
-**║┣⪼ Ⲧⲉⳑⲉⲏⲧⲟⲛ     ┣⪼  {version.__version__} \n"
-**║┣⪼ [✨🐍PYTHON┣⪼ 𝐔𝐬𝐞𝐫𝐛𝐨𝐭🐍✨](https://t.me/Python_Userbot_Support)\n"
-**║╰━━━━━━━━━━━━━━━➣ \n"
-**╚══════════════════❍⊱❁۪۪¤๋͜-  \n"L}
+    ♥️ ẞø✞ ẞ✞α✞µѕ ♥️
+**•⚜️•Øաղ̃ҽ̈r     : {mention}
+•🌹•𝖑𝖊ɠêɳ̃dẞø✞ : {LEGENDversion}
+•🌹•✞ҽ̀lҽ́ƭhøղ  : {version.version}
+•🌹•Ãbûßê     :  {abuse_m}
+•🌹•ßudø      :  {is_sudo}
+•🌹•Bøt.      : {Config.BOY_OR_GIRL}
 """
 botname = Config.BOT_USERNAME
 
 @bot.on(admin_cmd(pattern="alive$"))
 @bot.on(admin_cmd(pattern="alive$", allow_sudo=True))
-async def python_a(event):
+async def legend_a(event):
     try:
-        python = await bot.inline_query(botname, "alive")
-        await python[0].click(event.chat_id)
-        if event.sender_id == Legendl_Mr_Hacker:
+        legend = await bot.inline_query(botname, "alive")
+        await legend[0].click(event.chat_id)
+        if event.sender_id == Its_LegendBoy:
             await event.delete()
     except (noin, dedbot):
         await eor(event, msg)
 
 CmdHelp("alive").add_command(
     'bot', None, 'υѕє αи∂ ѕєє'
+).add_type(
+    "Official"
 ).add()
