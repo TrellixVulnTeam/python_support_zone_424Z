663c699ac958dc6c785ef5dbeeb2985357e961fd
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-10-31 05:21:55 -0700

    Update botalv.py

--- userbot/plugins/botalv.py
+++ userbot/plugins/botalv.py
@@ -22,22 +22,22 @@ python = bot.uid
 mention = f"[{DEFAULTUSER}](tg://user?id={python})"
 
 
-PM_IMG = "https://telegra.ph/file/71339ef5c1b34cffa6cb5.jpg"
+PM_IMG = "https://telegra.ph/file/00399ad38373f61c5a2ff.jpg"
 pm_caption ="**Pythonẞø✞︎ 𝙸𝚜 𝙾𝚗𝚕𝚒𝚗𝚎**\n\n"
 
 pm_caption += f"**┏━━︎♠️✞t͛ẞ̸ Pythonẞø✞︎♠️━━┓**\n"
-pm_caption += f"**┣🌷 𝙼𝚢 𝙼𝚊𝚜𝚝𝚎𝚛    : {mention}**\n"
-pm_caption += f"**┣🌷 𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗 : `{version.__version__}`**\n"
-pm_caption += f"**┣🌷 PYTHONẞø✞︎ : {PYTHONversion}**\n"
-pm_caption += f"**┣🌷 𝚂𝚞𝚍𝚘     : `{sudou}`**\n"
-pm_caption += f"**┣🌷 𝙾𝚠𝚗𝚎𝚛     : [python](https://t.me/Legend_Mr_Hacker)**\n"
-pm_caption += f"**┗━━━━━[♠️𝙶𝚛𝚘𝚞𝚙♠️](https://t.me/Legend_Userbot)━━━━━━━━┛**\n"
+pm_caption += f"**┣🔥 𝙼𝚢 𝙼𝚊𝚜𝚝𝚎𝚛    : {mention}**\n"
+pm_caption += f"**┣🔥 𝚃𝚎𝚕𝚎𝚝𝚑𝚘𝚗 : `{version.__version__}`**\n"
+pm_caption += f"**┣🔥 PYTHONBOT : {PYTHONversion}**\n"
+pm_caption += f"**┣🔥 𝚂𝚞𝚍𝚘     : `{sudou}`**\n"
+pm_caption += f"**┣🔥 𝙾𝚠𝚗𝚎𝚛     : [python](https://t.me/Legend_Mr_Hacker)**\n"
+pm_caption += f"**┗━━━━━[♠️𝙶𝚛𝚘𝚞𝚙♠️](https://t.me/Python_Userbot_Support)━━━━━━━━┛**\n"
 
-pm_caption += "    [✨яєρο✨](https://github.com/LEGEND-LX/PYTHONBOT-V9.0.8) 🔹 [📜ℓιϲєиѕє📜](https://github.com/LEGEND-LC/PYTHONBOT/blob/master/LICENSE)"
+pm_caption += "    [☠️яєρο☠️](https://github.com/LEGEND-LX/PYTHONBOT-V9.0.8) 🔹 [📜ℓιϲєиѕє📜](https://github.com/LEGEND-LX/PYTHONBOT/blob/master/LICENSE)"
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="bot$"))
-@bot.on(sudo_cmd(pattern="bot$", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="pybot$"))
+@bot.on(sudo_cmd(pattern="pybot$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
