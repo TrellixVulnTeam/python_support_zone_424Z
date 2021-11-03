38f1d881ed8e9f44972779465a263d29aca52b61
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-11-03 00:06:20 -0700

    Update bot_alive.py

--- userbot/plugins/assistant/bot_alive.py
+++ userbot/plugins/assistant/bot_alive.py
@@ -3,25 +3,25 @@ from . import *
 from userbot import ALIVE_NAME
 from userbot import bot
 
-LEGEND_USER = bot.me.first_name
-Its_LegendBoy = bot.uid
-legend_mention = f"[{LEGEND_USER}](tg://user?id={Its_LegendBoy})"
+PYTHON_USER = bot.me.first_name
+Legendl_Mr_Hacker = bot.uid
+python_mention = f"[{PYTHON_USER}](tg://user?id={Legendl_Mr_Hacker})"
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "PYTHON"
 
 
 PM_IMG = "https://telegra.ph/file/c26fc61e904476083baa7.jpg"
-pm_caption = f"⚜『Lêɠêɳ̃dẞø†』Is Ôñĺîne⚜ \n\n"
-pm_caption += f"Ôwñêř ~ 『{legend_mention}』\n"
+pm_caption = f"⚜『I N PYTHONBOT』Is Ôñĺîne⚜ \n\n"
+pm_caption += f"Ôwñêř ~ 『{python_mention}』\n"
 pm_caption += f"**╭──────────────**\n"
 pm_caption += f"┣Ťêlethon ~ `1.15.0` \n"
-pm_caption += f"┣『Lêɠêɳ̃dẞø†』~ `{LEGENDversion}` \n"
-pm_caption += f"┣Çhâññel ~ [Channel](https://t.me/Its_LegendBot)\n"
-pm_caption += f"┣**License** ~ [License v3.0](github.com/LEGEND-OS/LEGENBOT/blob/master/LICENSE)\n"
-pm_caption += f"┣Copyright ~ By [『Lêɠêɳ̃dẞø†』 ](https://t.me/Legend_Userbot)\n"
-pm_caption += f"┣Assistant ~ By [『Lêɠêɳ̃dẞøy』 ](https://t.me/Its_LegendBoy)\n"
+pm_caption += f"┣『Pythonẞø†』~ `{PYTHONversion}` \n"
+pm_caption += f"┣Çhâññel ~ [Channel](https://t.me/Python_Updata)\n"
+pm_caption += f"┣**License** ~ [License v3.0](github.com/LEGEND-LX/PYTHONBOT-V9.0.8/blob/master/LICENSE)\n"
+pm_caption += f"┣Copyright ~ By [『Pythonẞø†』 ](https://t.me/Python_Userbot_Support)\n"
+pm_caption += f"┣Assistant ~ By [『Legend-Lx』 ](https://t.me/Legendl_Mr_Hacker)\n"
 pm_caption += f"╰──────────────\n"
-pm_caption += f"       »»» [『Lêɠêɳ̃dẞø†』](https://t.me/Legend_Userbot) «««"
+pm_caption += f"       »»» [『Pythonẞø†』](https://t.me/Python_Userbot_Support) «««"
 
 
 # only Owner Can Use it
