0ff80b34f60370f6ae26104fda341ba3c4eac3f8
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-11-07 07:33:13 -0800

    Update animations2.py

--- userbot/modulespy/animations2.py
+++ userbot/modulespy/animations2.py
@@ -2,11 +2,11 @@ import asyncio
 from collections import deque
 
 from userbot import *
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from PYTHONBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 from userbot.Config import Config
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "PYTHON"
 
 
 @bot.on(admin_cmd(pattern=r"boxs$", outgoing=True))
