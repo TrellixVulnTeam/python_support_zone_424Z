774dc74ed88468e625491356fad61c08aa85aaa6
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-11-09 06:49:25 -0800

    Update funtxt.py

--- userbot/modulespy/funtxt.py
+++ userbot/modulespy/funtxt.py
@@ -1,6 +1,6 @@
 import nekos
 
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+from PYTHONBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot import CMD_HELP
 from userbot.cmdhelp import CmdHelp
 
@@ -35,29 +35,29 @@ async def payf(event):
 
 @bot.on(admin_cmd(pattern="cat$"))
 @bot.on(sudo_cmd(pattern="cat$", allow_sudo=True))
-async def hmm(LEGEND):
-    if LEGEND.fwd_from:
+async def hmm(PYTHON):
+    if PYTHON.fwd_from:
         return
     reactcat = nekos.textcat()
-    await edit_or_reply(LEGEND, reactcat)
+    await edit_or_reply(PYTHON, reactcat)
 
 
 @bot.on(admin_cmd(pattern="why$"))
 @bot.on(sudo_cmd(pattern="why$", allow_sudo=True))
-async def hmm(LEGEND):
-    if LEGEND.fwd_from:
+async def hmm(PYTHON):
+    if PYTHON.fwd_from:
         return
-    whyLEGEND = nekos.why()
-    await edit_or_reply(LEGEND, whyLEGEND)
+    whyPYTHON = nekos.why()
+    await edit_or_reply(PYTHON, whyPYTHON)
 
 
 @bot.on(admin_cmd(pattern="fact$"))
 @bot.on(sudo_cmd(pattern="fact$", allow_sudo=True))
-async def hmm(LEGEND):
-    if LEGEND.fwd_from:
+async def hmm(PYTHON):
+    if PYTHON.fwd_from:
         return
-    factLEGEND = nekos.fact()
-    await edit_or_reply(LEGEND, factLEGEND)
+    factPYTHON = nekos.fact()
+    await edit_or_reply(PYTHON, factPYTHON)
 
 
 CmdHelp("funtxts").add_command(
