8ace6f50e830056bb99e7890cdec198713b49f25
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-11-15 05:04:18 -0800

    Update packer.py

--- userbot/modulespy/packer.py
+++ userbot/modulespy/packer.py
@@ -3,12 +3,12 @@ import os
 import asyncio
 
 from userbot import CmdHelp
-from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
-from userbot import bot as LEGENDBOT
+from PYTHONBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from userbot import bot as PYTHONBOT
 
 
-@LEGENDBOT.on(admin_cmd(pattern=r"unpack", outgoing=True))
-@LEGENDBOT.on(sudo_cmd(pattern=r"unpack"))
+@PYTHONBOT.on(admin_cmd(pattern=r"unpack", outgoing=True))
+@PYTHONBOT.on(sudo_cmd(pattern=r"unpack"))
 async def _(event):
     b = await event.client.download_media(await event.get_reply_message())
     a = open(b, "r")
@@ -23,8 +23,8 @@ async def _(event):
     os.remove(b)
 
 
-@LEGENDBOT.on(admin_cmd(pattern="repack ?(.*)", outgoing=True))
-@LEGENDBOT.on(sudo_cmd(pattern="repack ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="repack ?(.*)", outgoing=True))
+@PYTHONBOT.on(sudo_cmd(pattern="repack ?(.*)", allow_sudo=True))
 async def _(event):
     a = await event.get_reply_message()
     input_str = event.pattern_match.group(1)
