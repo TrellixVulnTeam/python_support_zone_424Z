8172009c4994ef634e3f1baacccd316baadf00d5
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-11-16 15:39:17 +0530

    Add files via upload

--- userbot/plugins/sql_helper/snip_sql.py
+++ userbot/plugins/sql_helper/snip_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, LargeBinary, Numeric, UnicodeText
 
-from PYTHONBOT.plugins.sql_helper import BASE, SESSION
+from LEGENDBOT.plugins.sql_helper import BASE, SESSION
 
 
 class Snips(BASE):
