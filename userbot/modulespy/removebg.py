fc4b2e95d1de8b0517eaaae011f07230f6fdc674
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】 <87700009+LEGEND-LX@users.noreply.github.com>
2021-11-16 07:48:32 -0800

    Update removebg.py

--- userbot/modulespy/removebg.py
+++ userbot/modulespy/removebg.py
@@ -3,9 +3,9 @@ import requests
 import PIL.ImageOps
 from PIL import Image, ImageDraw, ImageFont
 
-from LEGENDBOT import CmdHelp
+from PYTHONBOT import CmdHelp
 from userbot.Config import Config
-from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from PYTHONBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 TEMP_DIR = os.environ.get("TEMP_DIR", "./temp/")
    
@@ -51,20 +51,20 @@ async def remove_background(event):
     message_id = await reply_id(event)
     if event.reply_to_msg_id and not input_str:
         reply_message = await event.get_reply_message()
-        LEGENDevent = await edit_or_reply(event, "`Analysing...`")
+        PYTHONevent = await edit_or_reply(event, "`Analysing...`")
         file_name = os.path.join(TEMP_DIR, "rmbg.png")
         try:
             await event.client.download_media(reply_message, file_name)
         except Exception as e:
-            await edit_or_reply(LEGENDevent, f"`{str(e)}`")
+            await edit_or_reply(PYTHONevent, f"`{str(e)}`")
             return
         else:
-            await LEGENDevent.edit("`Removing Background of this media`")
+            await PYTHONevent.edit("`Removing Background of this media`")
             file_name = convert_toimage(file_name)
             response = ReTrieveFile(file_name)
             os.remove(file_name)
     elif input_str:
-        LEGENDevent = await edit_or_reply(event, "`Removing Background of this media`")
+        PYTHONevent = await edit_or_reply(event, "`Removing Background of this media`")
         response = ReTrieveURL(input_str)
     else:
         await edit_or_reply(
@@ -73,15 +73,15 @@ async def remove_background(event):
         )
         return
     contentType = response.headers.get("content-type")
-    remove_bg_image = "LEGENDBOT.png"
+    remove_bg_image = "PYTHONBOT.png"
     if "image" in contentType:
-        with open("LEGENDBOT.png", "wb") as removed_bg_file:
+        with open("PYTHONBOT.png", "wb") as removed_bg_file:
             removed_bg_file.write(response.content)
     else:
-        await edit_or_reply(LEGENDevent, f"`{response.content.decode('UTF-8')}`")
+        await edit_or_reply(PYTHONevent, f"`{response.content.decode('UTF-8')}`")
         return
     if cmd == "srmbg":
-        file = convert_tosticker(remove_bg_image, filename="LEGENDBOT.webp")
+        file = convert_tosticker(remove_bg_image, filename="PYTHONBOT.webp")
         await event.client.send_file(
             event.chat_id,
             file,
@@ -95,7 +95,7 @@ async def remove_background(event):
             force_document=True,
             reply_to=message_id,
         )
-    await LEGENDevent.delete()
+    await PYTHONevent.delete()
 
 
 # this method will call the API, and return in the appropriate format
