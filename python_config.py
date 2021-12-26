76cd3eaca2c8099dfdb9952fc998c4f1b0199501
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2021-12-25 23:27:22 -0800

    Update python_config.py

--- python_config.py
+++ python_config.py
@@ -22,7 +22,7 @@ if ENV:
         # This is required for the hash to torrent file functionality to work.
         HASH_TO_TORRENT_API = os.environ.get("HASH_TO_TORRENT_API", "https://example.com/torrent/{}");
         # This is required for the @telegraph functionality.
-        TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "PYTHONBOT")
+        TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "DARK_PYTHONBOT")
         # Get a Free API Key from OCR.Space
         OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
         # Send .get_id in any group with all your administration bots (added)
@@ -152,7 +152,7 @@ if ENV:
         LYDIA_API = os.environ.get("LYDIA_API",None)
         PLUGIN_CHANNEL = os.environ.get("PLUGIN_CHANNEL", None)
         UPSTREAM_REPO = os.environ.get(
-            "UPSTREAM_REPO", "https://github.com/LEGEND-LX/PYTHONBOT-V9.0.8"
+            "UPSTREAM_REPO", "https://github.com/LEGEND-LX/DARK_PYTHONBOT"
         )
         PYTHON_STRING = os.environ.get("PYTHON_STRING", None)
         BOT_MODE = os.environ.get("BOT_MODE", "ON")
@@ -161,7 +161,7 @@ if ENV:
         FORCE_SUB = os.environ.get("FORCE_SUB", None)
         FORCE_CHANNEL_UN = os.environ.get("FORCE_CHANNEL_UN", None)
         FORCE_CHANNEL_ID = int(os.environ.get("FORCE_CHANNEL_ID", False))
-        EXTRA_PYTHONBOT = os.environ.get("EXTRA_PYTHONBOT-V9.0.8", -1001221881562)
+        EXTRA_PYTHONBOT = os.environ.get("EXTRA_DARK_PYTHONBOT", -1001221881562)
         PM_DATA = os.environ.get("PM_DATA", "ENABLE")
 
 else:
