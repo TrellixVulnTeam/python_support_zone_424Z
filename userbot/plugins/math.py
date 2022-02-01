caa827a2d9cb983f9bd2f01f2bdc80d0dcdd9cf0
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-01 07:25:39 -0800

    Update math.py

--- userbot/plugins/math.py
+++ userbot/plugins/math.py
@@ -5,70 +5,70 @@
 # NOW IN LEGENDBOT
 import math
 
-from LEGENDBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
+from PYTHONBOT.utils import admin_cmd, sudo_cmd, edit_or_reply
 
 from userbot import CmdHelp
-from userbot import bot as LEGENDBOT
+from userbot import bot as PYTHONBOT
 
 
-@LEGENDBOT.on(admin_cmd(pattern="sin ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="sin ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="sin ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="sin ?(.*)", allow_sudo=True))
 async def findsin(event):
     input_str = int(event.pattern_match.group(1))
     output = math.sin(input_str)
     await event.edit(f"**Value of Sin** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="cos ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="cos ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="cos ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="cos ?(.*)", allow_sudo=True))
 async def find_cos(event):
     input_str = int(event.pattern_match.group(1))
     output = math.cos(input_str)
     await event.edit(f"**Value of Cos** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="tan ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="tan ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="tan ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="tan ?(.*)", allow_sudo=True))
 async def find_tan(event):
     input_str = int(event.pattern_match.group(1))
     output = math.tan(input_str)
     await event.edit(f"**Value of Tan** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="cosec ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="cosec ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="cosec ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="cosec ?(.*)", allow_sudo=True))
 async def find_csc(event):
     input_str = float(event.pattern_match.group(1))
     output = mpmath.csc(input_str)
     await event.edit(f"**Value of Cosec** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="sec ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="sec ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="sec ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="sec ?(.*)", allow_sudo=True))
 async def find_sec(event):
     input_str = float(event.pattern_match.group(1))
     output = mpmath.sec(input_str)
     await event.edit(f"**Value of Sec** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="cot ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="cot ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="cot ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="cot ?(.*)", allow_sudo=True))
 async def find_cot(event):
     input_str = float(event.pattern_match.group(1))
     output = mpmath.cot(input_str)
     await event.edit(f"**Value of Cot** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="square ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="square ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="square ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="square ?(.*)", allow_sudo=True))
 async def square(event):
     input_str = float(event.pattern_match.group(1))
     output = input_str * input_str
     await event.edit(f"**Square of** `{input_str}`\n== `{output}`")
 
 
-@LEGENDBOT.on(admin_cmd(pattern="cube ?(.*)"))
-@LEGENDBOT.on(sudo_cmd(pattern="cube ?(.*)", allow_sudo=True))
+@PYTHONBOT.on(admin_cmd(pattern="cube ?(.*)"))
+@PYTHONBOT.on(sudo_cmd(pattern="cube ?(.*)", allow_sudo=True))
 async def cube(event):
     input_str = float(event.pattern_match.group(1))  # DANGEROUSJATT
     output = input_str * input_str * input_str
