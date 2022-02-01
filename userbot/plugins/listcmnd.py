104dfc535dff54fa337bacab6f0dc790542586e7
【★༒✞︎Îτß-ℓєɠεɳ∂-ℓх】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-02-01 07:17:23 -0800

    Update listcmnd.py

--- userbot/plugins/listcmnd.py
+++ userbot/plugins/listcmnd.py
@@ -2,12 +2,12 @@ import asyncio
 import io
 from userbot import ALIVE_NAME
 from . import *
-from userbot import bot as LEGENDBOT
-from LEGENDBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "LEGEND"
-LEGEND_logo = "LEGEND_logo1"
-@LEGENDBOT.on(admin_cmd(pattern=r"listcmnd"))
-@LEGENDBOT.on(sudo_cmd(pattern=r"listcmnd", allow_sudo=True))
+from userbot import bot as PYTHONBOT
+from PYTHONBOT.utils import admin_cmd, edit_or_reply, sudo_cmd
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "PYTHON"
+PYTHON_logo = "PYTHON_logo1"
+@PYTHONBOT.on(admin_cmd(pattern=r"listcmnd"))
+@PYTHONBOT.on(sudo_cmd(pattern=r"listcmnd", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -15,7 +15,7 @@ async def install(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     cmd = "ls userbot/plugins"
-    thumb = LEGEND_logo
+    thumb = PYTHON_logo
     process = await asyncio.create_subprocess_shell(
         cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
     )
@@ -23,11 +23,11 @@ async def install(event):
     o = stdout.decode()
     _o = o.split("\n")
     o = "\n".join(_o)
-    OUTPUT = f"List of Plugins in LEGENDBOT :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LEGENDSupport for help."
+    OUTPUT = f"List of Plugins in PYTHONBOT :- \n\n{o}\n\n<><><><><><><><><><><><><><><><><><><><><><><><>\nHELP:- If you want to know the commands for a plugin, do :- \n.plinfo <plugin name> without the < > brackets. \nJoin https://t.me/LEGENDSupport for help."
     if len(OUTPUT) > 69:
         with io.BytesIO(str.encode(OUTPUT)) as out_file:
             out_file.name = "cmd_list.text"
-            LEGEND_file = await bot.send_file(
+            PYTHON_file = await bot.send_file(
                 event.chat_id,
                 out_file,
                 force_document=True,
@@ -35,7 +35,7 @@ async def install(event):
                 thumb=thumb,
                 reply_to=reply_to_id,
             )
-            await edit_or_reply(LEGEND_file, f"Output Too Large. This is the file for the list of plugins in LEGENDBOT.\n\n**BY :-** {DEFAULTUSER}")
+            await edit_or_reply(PYTHON_file, f"Output Too Large. This is the file for the list of plugins in PYTHONBOT.\n\n**BY :-** {DEFAULTUSER}")
             await event.delete()
 
 from userbot.cmdhelp import CmdHelp
