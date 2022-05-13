a3138a18bb917291742c04d65c32e2576921c1f8
【★༒✞︎©༒ʟɛɢɛռɖ✘-թπο】ρɾαωαรɦ-Malviya <87700009+LEGEND-LX@users.noreply.github.com>
2022-05-13 06:55:18 -0700

    Update string_session.py

--- string_session.py
+++ string_session.py
@@ -1,10 +1,10 @@
 
 from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
-PYTHONBOT = """
+SATYABOT = """
 """
-print(PYTHONBOT)
-print("""String Generator. ==> PYTHONBot. Get Your Api Id & Api Hash From my.telegram.org and fill accordingly.
+print(SATYABOT)
+print("""String Generator. ==> SATYABot. Get Your Api Id & Api Hash From my.telegram.org and fill accordingly.
 ╭━━━┳╮╱╱╭┳━━━━┳╮╱╭┳━━━┳━╮╱╭╮
 ┃╭━╮┃╰╮╭╯┃╭╮╭╮┃┃╱┃┃╭━╮┃┃╰╮┃┃
 ┃╰━╯┣╮╰╯╭┻╯┃┃╰┫╰━╯┃┃╱┃┃╭╮╰╯┃
@@ -20,13 +20,13 @@ print("""String Generator. ==> PYTHONBot. Get Your Api Id & Api Hash From my.tel
 APP_ID = int(input("Enter APP ID - "))
 API_HASH = input("Enter API HASH - ")
 
-with TelegramClient(StringSession(), APP_ID, API_HASH) as PYTHONBOT:
+with TelegramClient(StringSession(), APP_ID, API_HASH) as SATYABOT:
     print("")
     print("This is your STRING_SESSION. Please Keep It safe.")
     print("")
-    print(PYTHONBOT.session.save())
+    print(SATYABOT.session.save())
     print("")
     print("You can Get Your String Session In Saved Message of Your Telegram Account. Remember To Make New String Session Whenever You Terminate Sessions.")
-    omk =PYTHONBOT.send_message("me", f"`{PYTHONBOT.session.save()}`")
-    omk.reply("The above is the `PYTHON_STRING` #POWERFUL [PYTHONBOT](https://t.me/Python_Userbot_Support)"
+    omk =SATYABOT.send_message("me", f"`{SATYABOT.session.save()}`")
+    omk.reply("The above is the `SATYA_STRING` #POWERFUL [SATYABOT](https://t.me/Python_Userbot_Support)"
 		)
